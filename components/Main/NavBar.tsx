7406836e65c3a43fb0761a4f26bffe714f142ef9
Leirbag <gomesrdg34@gmail.com>
2024-02-04 03:03:18 -0300

    site

--- components/Main/NavBar.tsx
+++ components/Main/NavBar.tsx
@@ -7,7 +7,7 @@ const NavBar = () => {
         <div className='w-full h-[65px] fixed top-0 shadow-lg shadow-[#176280f8]/50 bg-[#00000000] backdrop-blur-md z-50 px-10'>
             <div className='w-full h-full flex flex-row items-center justify-between mt-auto px-[10px]'>
                 <a href="/" className='h-auto w-auto flex flex-row items-center'>
-                    <Image src='/NavLogo.png' alt='logo' width={70} height={70} className='cursor-pointer hover:animate-slowspin' />
+                    <Image src='/GG.png' alt='logo' width={70} height={70} className='cursor-pointer hover:animate-slowspin' />
 
                     <span className='font-bold ml-[10px] hidden md:block text-gray-300'>
                         Leirbaggr
--- components/Main/Projects.tsx
+++ components/Main/Projects.tsx
@@ -8,81 +8,69 @@ const Projects = () => {
                 Meus Projetos
             </h1>
             <div className='h-full w-full flex flex-col md:flex-row gap-10 px-10'>
-                <div className=' hover:scale-105 transition-all'>
-                    <ProjectsCard
-                        src='/ecommerce.png'
-                        title='LeirbaggShop'
-                        description='Site de ecommerce, esse foi um projeto feito para uma avaliação no Senac Minas, um ecommerce completo e funcional.'
-                    />
-                </div>
 
-                <div className=' hover:scale-105 transition-all'>
-                    <ProjectsCard
-                        src='/Lexicorm.png'
-                        title='Lexicorm Company'
-                        description='A lexicorm foi desenvolvida para simplificar a busca por informações sobre medicamentos, tornando-a mais prática e acessível.'
-                    />
-                </div>
+                <ProjectsCard
+                    src='/ecommerce.png'
+                    title='LeirbaggShop'
+                    description='Site de ecommerce, esse foi um projeto feito para uma avaliação no Senac Minas, um ecommerce completo e funcional.'
+                />
 
-                <div className=' hover:scale-105 transition-all'>
-                    <ProjectsCard
-                        src='/coffee.png'
-                        title='Coffee'
-                        description='É um site destinado para a venda de cafés, sua interface é simples, porém não deixa de ser um linda nas cores e destalhes.'
-                    />
-                </div>
+                <ProjectsCard
+                    src='/Lexicorm.png'
+                    title='Lexicorm Company'
+                    description='A lexicorm foi desenvolvida para simplificar a busca por informações sobre medicamentos, tornando-a mais prática e acessível.'
+                />
+
+                <ProjectsCard
+                    src='/coffee.png'
+                    title='Coffee'
+                    description='É um site destinado para a venda de cafés, sua interface é simples, porém não deixa de ser um linda nas cores e destalhes.'
+                />
+
             </div>
 
             <div className='h-full w-full flex flex-col mt-5 md:flex-row gap-10 px-10'>
-                <div className=' hover:scale-105 transition-all'>
-                    <ProjectsCard
-                        src='/Home.png'
-                        title='Nike'
-                        description='Esse é um projeto feito no figma, um protótipo do site da Nike para fins de estudos da plataforma.'
-                    />
-                </div>
 
-                <div className=' hover:scale-105 transition-all'>
-                    <ProjectsCard
-                        src='/Cart.png'
-                        title='Nike | Carrinho'
-                        description='Esse é um projeto feito no figma, um protótipo do site da Nike para fins de estudos da plataforma.'
-                    />
-                </div>
+                <ProjectsCard
+                    src='/Home.png'
+                    title='Nike'
+                    description='Esse é um projeto feito no figma, um protótipo do site da Nike para fins de estudos da plataforma.'
+                />
 
-                <div className=' hover:scale-105 transition-all'>
-                    <ProjectsCard
-                        src='/controllers.png'
-                        title='Controllers'
-                        description='Uma landing page para venda de controle de video game, projeto feito no figma, um protótipo para fins de estudos.'
-                    />
-                </div>
+                <ProjectsCard
+                    src='/Cart.png'
+                    title='Nike | Carrinho'
+                    description='Esse é um projeto feito no figma, um protótipo do site da Nike para fins de estudos da plataforma.'
+                />
+
+                <ProjectsCard
+                    src='/controllers.png'
+                    title='Controllers'
+                    description='Uma landing page para venda de controle de video game, projeto feito no figma, um protótipo para fins de estudos.'
+                />
+
             </div>
 
             <div className='h-full w-full flex flex-col mt-5 md:flex-row gap-10 px-10'>
-                <div className=' hover:scale-105 transition-all'>
-                    <ProjectsCard
-                        src='/Details.png'
-                        title='Air Max'
-                        description='Uma landing page para vendas dos tênis da Nike, layout inspirado nos estilos da Nike totalmente otimizado para computadores.'
-                    />
-                </div>
 
-                <div className=' hover:scale-105 transition-all'>
-                    <ProjectsCard
-                        src='/Details 2.png'
-                        title='Lebron'
-                        description='Uma landing page para vendas dos tênis da Nike, layout inspirado nos estilos da Nike totalmente otimizado para computadores.'
-                    />
-                </div>
+                <ProjectsCard
+                    src='/Details.png'
+                    title='Air Max'
+                    description='Uma landing page para vendas dos tênis da Nike, layout inspirado nos estilos da Nike totalmente otimizado para computadores.'
+                />
 
-                <div className=' hover:scale-105 transition-all'>
-                    <ProjectsCard
-                        src='/Details 3.png'
-                        title='Lebron Witness 8'
-                        description='Uma landing page para vendas dos tênis da Nike, layout inspirado nos estilos da Nike totalmente otimizado para computadores.'
-                    />
-                </div>
+                <ProjectsCard
+                    src='/Details 2.png'
+                    title='Lebron'
+                    description='Uma landing page para vendas dos tênis da Nike, layout inspirado nos estilos da Nike totalmente otimizado para computadores.'
+                />
+
+                <ProjectsCard
+                    src='/Details 3.png'
+                    title='Lebron Witness 8'
+                    description='Uma landing page para vendas dos tênis da Nike, layout inspirado nos estilos da Nike totalmente otimizado para computadores.'
+                />
+
             </div>
         </div>
     )
